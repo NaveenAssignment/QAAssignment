5a2101c138cc26e5060a2d33797bcd1b1615d03c
Aditya <Aditya@Aadi>
2017-03-07 17:37:24 +0530

    done

--- src/com/assignment/listOfPlayers.java
+++ src/com/assignment/listOfPlayers.java
@@ -7,14 +7,17 @@ import java.util.concurrent.TimeUnit;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 /*check to see if they show up after clicking “Filter Plays” on this page . If ALL of the filters listed below show up, then the test passes*/
 public class listOfPlayers {
 
 	public static void main(String[] args) {
 		
-		System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.13.0-win64\\geckodriver.exe");
-		WebDriver driver = new FirefoxDriver();
+		/*System.setProperty("webdriver.gecko.driver", "C:\\geckodriver-v0.13.0-win64\\geckodriver.exe");
+		WebDriver driver = new FirefoxDriver();*/
+		System.setProperty("webdriver.chrome.driver","D:\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.get("https://v2-pre-prod-app.krossover.com/intelligence/games/9603/breakdown");
